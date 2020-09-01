720d797f8e506dd8e7dee70f2800b96ebb2dcba3
Tom Pope <popetr90@gmail.com>
2020-09-01 12:05:40 +0100

    added emdbedded video and navbar to homepage

--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,12 @@ Rails.application.routes.draw do
   namespace :manager do
     resources :dashboard
   end
+
+  get "/about", to: "pages#about"
+  get "/flowers", to: "pages#flowers"
+  get "/contact", to: "pages#contact"
+  get "/feedback", to: "pages#new_feedback"
+  post "/feedback/:id", to: "pages#create_feedback"
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
