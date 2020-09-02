2f28ddbb40ef3f8c49dfd14f1ef039994165db33
Florian Bucher <bucher.fl1@gmail.com>
2020-09-02 11:29:24 +0200

    Added Delivery_date, delivery_day and time_of_day to flower subscriptions

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_31_162134) do
+ActiveRecord::Schema.define(version: 2020_09_02_091745) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,9 @@ ActiveRecord::Schema.define(version: 2020_08_31_162134) do
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.datetime "delivery_date"
+    t.string "delivery_day"
+    t.string "time_of_day"
     t.index ["user_id"], name: "index_flower_subscriptions_on_user_id"
   end
 
