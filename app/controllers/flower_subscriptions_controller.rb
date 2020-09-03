aa3ac2044064eb8f8f2b243e2f3bc451a2ee8729
Tom Pope <popetr90@gmail.com>
2020-09-03 10:36:22 +0100

    fixing authentication on pages and flower sub controller

--- app/controllers/flower_subscriptions_controller.rb
+++ app/controllers/flower_subscriptions_controller.rb
@@ -1,4 +1,6 @@
 class FlowerSubscriptionsController < ApplicationController
+  skip_before_action :authenticate_user!, only: [ :index]
+
   def index
     @flower_subscriptions = FlowerSubscription.all
   end
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :home ]
+  skip_before_action :authenticate_user!, only: [ :home, :flowers, :about, :new_feedback, :create_feedback ]
 
   def home
   end
