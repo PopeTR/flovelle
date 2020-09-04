fa298420ade07f7aabf0db768875940e224c1435
Tom Pope <popetr90@gmail.com>
2020-09-04 15:54:42 +0100

    editing jurians changes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,13 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
-  before_action :configure_devise_params, if: :devise_controller?
-  def after_sign_in_path_for(resource)
-    if request.referer == "http://localhost:3000/flower_subscriptions"
-      "/flower_subscriptions"
-    else
-      request.referer
-    end
-  end
+  # before_action :configure_devise_params, if: :devise_controller?
+  # def after_sign_in_path_for(resource)
+  #   if request.referer == "http://localhost:3000/flower_subscriptions"
+  #     "/flower_subscriptions"
+  #   else
+  #     request.referer
+  #   end
+  # end
 
 
 end
