694801df2650639b0adcabd6650e3c19ac2e0820
Tom Pope <popetr90@gmail.com>
2020-09-07 16:35:27 +0100

    dashboard, account page

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -2,6 +2,8 @@ class AccountsController < ApplicationController
   def index
     @bookings = current_user.flower_subscriptions
     @order = current_user.flower_subscriptions.first
+    @last_delivery = @order.delivery_date
+    @delivery = delivery_check
   end
 
   def show
@@ -11,5 +13,26 @@ class AccountsController < ApplicationController
   end
 
   def edit
+  end
+
+  def delivery_check
+    if Date.today > @last_delivery
+      next_delivery
+    else
+      @delivery = @last_delivery
+    end
+
+  end
+
+  def next_delivery
+    if @order.frequency == 1
+      @next_delivery = @last_delivery + 28.days
+    elsif @order.frequency == 2
+      @next_delivery = @last_delivery + 14.days
+
+    elsif @order.frequency == 4
+      @next_delivery = @last_delivery + 7.days
+    end
+    @last_delivery = @next_delivery
   end
 end
